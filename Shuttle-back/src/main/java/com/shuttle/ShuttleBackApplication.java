afb3c8ab059a0e451da88e3c1c8bc0fa6b81b59c
magley <60552482+magley@users.noreply.github.com>
2022-11-26 12:02:35 +0100

    feat: Make helloWorld() accessible from Angular origin

--- Shuttle-back/src/main/java/com/shuttle/ShuttleBackApplication.java
+++ Shuttle-back/src/main/java/com/shuttle/ShuttleBackApplication.java
@@ -2,6 +2,7 @@ package com.shuttle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,8 @@ public class ShuttleBackApplication {
 	}
 	
 	@GetMapping("/hello")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public String helloWorld() {
-		return "Hello World!";
+		return "Hello from Spring Boot";
 	}
 }
