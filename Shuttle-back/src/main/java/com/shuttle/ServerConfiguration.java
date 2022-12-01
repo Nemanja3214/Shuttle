2d452af5a88d588b6fb0f167ee30f26cbf372a79
magley <magazinlazar2@gmail.com>
2022-12-01 13:17:03 +0100

    broken: Add global CORS mapping
    Swagger UI does not show up once this class is created.

---
+++ Shuttle-back/src/main/java/com/shuttle/ServerConfiguration.java
@@ -0,0 +1,15 @@
+package com.shuttle;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ServerConfiguration implements WebMvcConfigurer  {
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
