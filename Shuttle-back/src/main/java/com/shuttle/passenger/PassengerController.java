63bb284673f34d9d4021c1ac5dd06ae39cee5597
magley <60552482+magley@users.noreply.github.com>
2022-12-04 11:31:20 +0100

    feat: Implement GET /api/passenger

--- Shuttle-back/src/main/java/com/shuttle/passenger/PassengerController.java
+++ Shuttle-back/src/main/java/com/shuttle/passenger/PassengerController.java
@@ -1,13 +1,19 @@
 package com.shuttle.passenger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class PassengerController {
@@ -17,4 +23,14 @@ public class PassengerController {
     	passenger.setId(Long.valueOf(123));
         return new ResponseEntity<>(new PassengerDTO(passenger), HttpStatus.OK);
     }
+    
+    @CrossOrigin
+    @GetMapping("/api/passenger")
+    public ResponseEntity<PassengerPageDTO> getPaginated(@PathParam("page") int page, @PathParam("size") int size) {
+    	List<Passenger> passengersMock = new ArrayList<>();
+    	passengersMock.add(new Passenger());
+    	passengersMock.add(new Passenger());
+    	
+        return new ResponseEntity<>(new PassengerPageDTO(passengersMock), HttpStatus.OK);
+    }    
 }
