2e45f3b0c38ffb5b5c2a7f2d235940b7eb3e3a93
magley <60552482+magley@users.noreply.github.com>
2022-12-04 11:36:40 +0100

    feat: Implement GET /api/passenger{id}

--- Shuttle-back/src/main/java/com/shuttle/passenger/PassengerController.java
+++ Shuttle-back/src/main/java/com/shuttle/passenger/PassengerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +18,12 @@ import jakarta.websocket.server.PathParam;
 
 @RestController
 public class PassengerController {
-    @CrossOrigin
     @PostMapping("/api/passenger")
     public ResponseEntity<PassengerDTO> create(@RequestBody Passenger passenger) {
     	passenger.setId(Long.valueOf(123));
         return new ResponseEntity<>(new PassengerDTO(passenger), HttpStatus.OK);
     }
     
-    @CrossOrigin
     @GetMapping("/api/passenger")
     public ResponseEntity<PassengerPageDTO> getPaginated(@PathParam("page") int page, @PathParam("size") int size) {
     	List<Passenger> passengersMock = new ArrayList<>();
@@ -32,5 +31,13 @@ public class PassengerController {
     	passengersMock.add(new Passenger());
     	
         return new ResponseEntity<>(new PassengerPageDTO(passengersMock), HttpStatus.OK);
-    }    
+    }
+    
+    @GetMapping("/api/passenger/{id}")
+    public ResponseEntity<PassengerDTO> getDetails(@PathVariable("id") Long id) {
+    	Passenger passenger = new Passenger();
+    	passenger.setId(id);
+    	
+    	return new ResponseEntity<>(new PassengerDTO(passenger), HttpStatus.OK);
+    }
 }
