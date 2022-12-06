591d5ff9f47a765b6a0d8e636f15150ad160ac04
magley <magazinlazar2@gmail.com>
2022-12-06 17:48:23 +0100

    feat: Fix redundant fields in RouteDTO
    As per the new OpenAPI specification

--- Shuttle-back/src/main/java/com/shuttle/location/RouteDTO.java
+++ Shuttle-back/src/main/java/com/shuttle/location/RouteDTO.java
@@ -1,16 +1,10 @@
 package com.shuttle.location;
 
 public class RouteDTO {
-	public String address;
-	public Double latitude;
-	public Double longitude;
 	public LocationDTO departure;
 	public LocationDTO destination;
 	
 	public RouteDTO(LocationDTO departure, LocationDTO destination) {
-		this.address = departure.address;
-		this.latitude = departure.latitude;
-		this.longitude = departure.longitude;
 		this.departure = departure;
 		this.destination = destination;
 	}
