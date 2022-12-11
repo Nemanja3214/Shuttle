6c47a0546c9656ac214558444b6bccc81d1f3edb
magley <60552482+magley@users.noreply.github.com>
2022-12-11 13:29:22 +0100

    fix: Partial merge conflict resolution
    Some things require manual adjusting after the merge.

--- Shuttle-back/src/main/java/com/shuttle/ride/dto/RejectionDTO.java
+++ Shuttle-back/src/main/java/com/shuttle/ride/dto/RejectionDTO.java
@@ -4,11 +4,41 @@ import java.time.LocalDateTime;
 import com.shuttle.ride.Rejection;
 
 public class RejectionDTO {
-	public String reason;
-	public LocalDateTime timeOfRejection;
-	
+	private String reason;
+	private LocalDateTime timeOfRejection;
+
 	public RejectionDTO(Rejection rej) {
 		this.reason = rej.getReason();
 		this.timeOfRejection = rej.getTimeOfRejection();
+	}
+
+	public RejectionDTO() {
+		super();
+	}
+
+	public RejectionDTO(String reason, LocalDateTime timeOfRejection) {
+		super();
+		this.reason = reason;
+		this.timeOfRejection = timeOfRejection;
+	}
+
+	public static RejectionDTO getMock() {
+		return new RejectionDTO("Ride is canceled due to previous problems with the passenger", LocalDateTime.now());
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public LocalDateTime getTimeOfRejection() {
+		return timeOfRejection;
+	}
+
+	public void setTimeOfRejection(LocalDateTime timeOfRejection) {
+		this.timeOfRejection = timeOfRejection;
 	}
 }
--- Shuttle-back/src/main/java/com/shuttle/ride/dto/RideDTO.java
+++ Shuttle-back/src/main/java/com/shuttle/ride/dto/RideDTO.java
@@ -14,18 +14,19 @@ import com.shuttle.ride.Ride;
 import com.shuttle.vehicle.Vehicle;
 
 public class RideDTO {
-	public Long id;
-	public List<RouteDTO> locations;
-	public String startTime;
-	public String endTime;
-	public Integer totalCost;
-	public RideDriverDTO driver;
-	public List<RidePassengerDTO> passengers;
-	public Integer estimatedTimeInMinutes;
-	public Boolean babyTransport;
-	public Boolean petTransport;
-	public Vehicle.Type vehicleType;
-	public RejectionDTO rejection;
+	private Long id;
+	private List<RouteDTO> locations;
+	private String startTime;
+	private String endTime;
+	private Integer totalCost;
+	private RideDriverDTO driver;
+	private List<RidePassengerDTO> passengers;
+	private Integer estimatedTimeInMinutes;
+	private Boolean babyTransport;
+	private Boolean petTransport;
+	private Vehicle.Type vehicleType;
+	private RejectionDTO rejection;
+	private String status;
 	
 	public RideDTO(Ride ride) {
 		this.id = ride.getId();
@@ -50,4 +51,135 @@ public class RideDTO {
 		this.vehicleType = ride.getVehicleType();
 		this.rejection = new RejectionDTO(new Rejection());
 	}
+	
+	public RideDTO() {
+		super();
+	}
+	
+	public RideDTO(long id, LocalDateTime startTime, LocalDateTime endTime, Integer totalCost, RideDriverDTO driver,
+			List<RidePassengerDTO> passengers, int estimatedTimeInMinutes, Vehicle.Type vehicleType, boolean babyTransport,
+			boolean petTransport, RejectionDTO rejection, List<RouteDTO> locations, String status) {
+		super();
+		this.id = id;
+		this.startTime = startTime.format(DateTimeFormatter.ISO_DATE_TIME);
+		this.endTime = endTime.format(DateTimeFormatter.ISO_DATE_TIME);
+		this.totalCost = totalCost;
+		this.driver = driver;
+		this.passengers = passengers;
+		this.estimatedTimeInMinutes = estimatedTimeInMinutes;
+		this.vehicleType = vehicleType;
+		this.babyTransport = babyTransport;
+		this.petTransport = petTransport;
+		this.rejection = rejection;
+		this.locations = locations;
+		this.status = status;
+	}
+
+//	public static RideDTO getMock() {
+//		List<LocationPairDTO> locations = new ArrayList<>();
+//		locations.add(LocationPairDTO.getMock());
+//		
+//		List<BasicUserInfoDTO> passengers = new ArrayList<>();
+//		passengers.add(BasicUserInfoDTO.getMock());
+//		return new RideDTO(123, LocalDateTime.now(), LocalDateTime.now(), 1260, BasicUserInfoDTO.getMock(), passengers, 5, "STANDARDNO",
+//				true, true, RejectionDTO.getMock(), locations, "PENDING");
+//	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<RouteDTO> getLocations() {
+		return locations;
+	}
+
+	public void setLocations(List<RouteDTO> locations) {
+		this.locations = locations;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public Integer getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(Integer totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public RideDriverDTO getDriver() {
+		return driver;
+	}
+
+	public void setDriver(RideDriverDTO driver) {
+		this.driver = driver;
+	}
+
+	public List<RidePassengerDTO> getPassengers() {
+		return passengers;
+	}
+
+	public void setPassengers(List<RidePassengerDTO> passengers) {
+		this.passengers = passengers;
+	}
+
+	public Integer getEstimatedTimeInMinutes() {
+		return estimatedTimeInMinutes;
+	}
+
+	public void setEstimatedTimeInMinutes(Integer estimatedTimeInMinutes) {
+		this.estimatedTimeInMinutes = estimatedTimeInMinutes;
+	}
+
+	public Boolean getBabyTransport() {
+		return babyTransport;
+	}
+
+	public void setBabyTransport(Boolean babyTransport) {
+		this.babyTransport = babyTransport;
+	}
+
+	public Boolean getPetTransport() {
+		return petTransport;
+	}
+
+	public void setPetTransport(Boolean petTransport) {
+		this.petTransport = petTransport;
+	}
+
+	public Vehicle.Type getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(Vehicle.Type vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+	public RejectionDTO getRejection() {
+		return rejection;
+	}
+
+	public void setRejection(RejectionDTO rejection) {
+		this.rejection = rejection;
+	}
+	
+	
 }
