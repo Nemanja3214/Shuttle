aaa76e01d173c7d78860b3fd10b6b934ccac9c4e
magley <magazinlazar2@gmail.com>
2022-12-16 10:10:31 +0100

    feat: Add vehicle type insertions into data.sql

--- Shuttle-back/src/main/java/com/shuttle/vehicle/VehicleType.java
+++ Shuttle-back/src/main/java/com/shuttle/vehicle/VehicleType.java
@@ -4,16 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jdk.jfr.Enabled;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "vehicle_type")
 public class VehicleType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     private String name;
     private double pricePerKM;
-
 }
---
+++ Shuttle-back/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Vehicle types are hardcoded at the start and never changed.
+-- Don't remove this!
+
+insert into vehicle_type(name, price_PerKM) values ('Standard', 40)
+insert into vehicle_type(name, price_PerKM) values ('Luxury', 80)
+insert into vehicle_type(name, price_PerKM) values ('Van', 60)
