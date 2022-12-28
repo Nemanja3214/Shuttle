8ffa7272f3ccc50d35f28e78c4804a3357134e52
uros <urosstanic25@gmail.com>
2022-12-28 22:56:08 +0100

    fixed IRideRepository

--- Shuttle-back/src/main/java/com/shuttle/ride/IRideRepository.java
+++ Shuttle-back/src/main/java/com/shuttle/ride/IRideRepository.java
@@ -1,10 +1,12 @@
 package com.shuttle.ride;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.shuttle.driver.Driver;
 
