fbfa68d96f5dc1214f6d5c384015ccff78a83fbe
magley <60552482+magley@users.noreply.github.com>
2023-01-11 17:54:09 +0100

    feat: Add unregistered user endpoint validation

--- Shuttle-back/src/main/java/com/shuttle/ride/dto/CreateRideEstimationDTO.java
+++ Shuttle-back/src/main/java/com/shuttle/ride/dto/CreateRideEstimationDTO.java
@@ -1,63 +1,26 @@
 package com.shuttle.ride.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.shuttle.location.dto.LocationDTO;
 import com.shuttle.location.dto.RouteDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class CreateRideEstimationDTO {
 	private List<RouteDTO> locations;
 	private String vehicleType;
-	private boolean babyTransport;
-	private boolean petTransport;
+	private Boolean babyTransport;
+	private Boolean petTransport;
 	private long travelTime;
-
-	public List<RouteDTO> getLocations() {
-		return locations;
-	}
-
-	public void setLocations(List<RouteDTO> locations) {
-		this.locations = locations;
-	}
-
-	public String getVehicleType() {
-		return vehicleType;
-	}
-
-	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
-	}
-
-	public boolean isBabyTransport() {
-		return babyTransport;
-	}
-
-	public void setBabyTransport(boolean babyTransport) {
-		this.babyTransport = babyTransport;
-	}
-
-	public boolean isPetTransport() {
-		return petTransport;
-	}
-
-	public void setPetTransport(boolean petTransport) {
-		this.petTransport = petTransport;
-	}
-
-
-	public long getTravelTime() {
-		return travelTime;
-	}
-
-	public void setTravelTime(long travelTime) {
-		this.travelTime = travelTime;
-	}
 
 	public double calculateLength() {
 		return locations.stream().map(route -> route.getDistance()).collect(Collectors.summingDouble(Double::doubleValue));
 	}
-	
-
 }
