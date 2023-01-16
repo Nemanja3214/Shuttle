615de769dabbb02c1a947a8b12576bc7ec3bdb79
SV 10/2020 <nemanja.majstorovic3214@gmail.com>
2023-01-16 23:11:10 +0100

    Refactor removed extra

--- Shuttle-back/src/main/java/com/shuttle/common/FileUploadUtil.java
+++ Shuttle-back/src/main/java/com/shuttle/common/FileUploadUtil.java
@@ -29,7 +29,6 @@ public class FileUploadUtil {
             Files.createDirectories(uploadPath);
         }
          
-//        BufferedImage image = null;
         if(imageBase64 == null) {
         	return;
         }
@@ -41,16 +40,12 @@ public class FileUploadUtil {
           	throw new InvalidBase64Exception();
 		}
 
-//        image = ImageIO.read(new ByteArrayInputStream(imageByte));
-        
-//        File outputfile = new File(uploadDir + fileName);
-//        if(image == null) {
-//        	throw new InvalidBase64Exception();
-//        }
         try (OutputStream stream = new FileOutputStream(uploadDir + fileName + ".png")) {
             stream.write(imageByte);
         }
-//        ImageIO.write(image, "png", outputfile);
+        catch(Exception e) {
+        	throw new InvalidBase64Exception();
+        }
     }
 
 	public static String getImageBase64(String uploadDir,String pictureName) throws IOException, NonExistantImageException {
@@ -92,24 +87,6 @@ public class FileUploadUtil {
 		}
 		imageSize /= (1000 * 1000);
 		return imageSize;
-	}
-	
-	public static void main(String[] args) {
-		String picture = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+L+U4T8ABu8CpCYJ1DQAAAAASUVORK5CYII=";
-		try {
-			saveFile(documentPictureUploadDir, "asd", picture);
-			String result = getImageBase64(documentPictureUploadDir, "asd.png");
-			System.out.println(picture.equals(result));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidBase64Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NonExistantImageException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
 	}
 
 }
