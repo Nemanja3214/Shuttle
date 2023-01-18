350cd2687615ee175d60a70f2d2fc716fb0eda83
magley <60552482+magley@users.noreply.github.com>
2023-01-18 17:45:00 +0100

    feat: Allow access to /h2-console and change password hash endpoint

--- Shuttle-back/src/main/java/com/shuttle/SwaggerController.java
+++ Shuttle-back/src/main/java/com/shuttle/SwaggerController.java
@@ -5,6 +5,7 @@ import com.shuttle.user.GenericUser;
 import com.shuttle.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +17,7 @@ public class SwaggerController {
     RoleService roleService;
     @Autowired
     UserService userService;
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/password-hash")
     public void index() {
         for (GenericUser user :
                 userService.findAll()) {
