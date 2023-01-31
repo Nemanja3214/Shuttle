ba0bfd78f2e5e0247af060af95f0a6c433a7377d
magley <60552482+magley@users.noreply.github.com>
2023-01-31 10:50:48 +0100

    chore: Add partial 2-webdriver test

---
+++ Shuttle-Selenium/src/test/java/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import util.Util;
+
+public class LoginPage {
+	private WebDriver driver;
+	
+	public LoginPage(WebDriver webdriver) {
+		this.driver = webdriver;
+		driver.get(Util.ShuttleURL);
+        PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(how = How.CSS, using = "#login")
+    private WebElement btnToolbarLogin;
+	
+	@FindBy(how = How.CSS, using = "#login-email")
+	private WebElement inputEmail;
+	
+	@FindBy(how = How.CSS, using = "#login-password")
+	private WebElement inputPassword;
+	
+	@FindBy(how = How.CSS, using = "#login-error")
+	private WebElement divLoginError;
+	
+	@FindBy(how = How.CSS, using = "#submitButton")
+	private WebElement btnSubmitLogin;
+	
+	public void btnToolbarLogin_click() {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(btnToolbarLogin)).click();
+	}
+	
+	public void login(String email, String password) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOf(inputEmail)).clear();
+		inputEmail.sendKeys(email);
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOf(inputPassword)).clear();
+		inputPassword.sendKeys(password);
+		
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(btnSubmitLogin)).click();
+	}
+	
+	public String getLoginErrorText() {
+		String txt = (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOf(inputEmail)).getText();
+		return txt;
+	}
+}
--- Shuttle-Selenium/src/test/java/test/BasicTest.java
+++ Shuttle-Selenium/src/test/java/test/BasicTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -12,34 +14,67 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import pages.HomePage;
+import pages.LoginPage;
+import pages.PassengerHome;
 import util.DriverSetup;
 import util.Util;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class BasicTest {
 	private static WebDriver webdriver;
+	private static WebDriver webdriver2;
 	
 	@BeforeAll
 	public void init() {
 		webdriver = DriverSetup.useEdge();
 		webdriver.manage().window().maximize();
+		
+		webdriver2 = DriverSetup.useEdge();
+		webdriver2.manage().window().maximize();
 	}
 	
 	@AfterAll
 	public void fini() {
 		webdriver.close();
+		webdriver2.close();
 	}
-	
+
 	@Test
-	public void basicTest() {
-		HomePage homePage = new HomePage(webdriver);
-		homePage.BtnToolbarLogin_click();
-		homePage.Login("bob@gmail.com", "bob123");
+	public void loginTestBadCredentials() {
+		LoginPage loginPage = new LoginPage(webdriver);
+		loginPage.btnToolbarLogin_click();
 		
-		Util.takeScreenshoot(webdriver, "login_form_bob_entered");
+		loginPage.login("bob@gmail.com", "b");
+		String err = loginPage.getLoginErrorText();
+		assertThat(err.equals("Wrong username or password!"));
+		
+		loginPage.login("b@gmail.com", "bob112");
+		err = loginPage.getLoginErrorText();
+		assertThat(err.equals("Wrong username or password!"));
 		
 		try {
 			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testWithTwoUsersAndPassengerEntersRideDetails() {
+		LoginPage loginPage1 = new LoginPage(webdriver);	// webdriver  = PASSENGER
+		LoginPage loginPage2 = new LoginPage(webdriver2);	// webdriver2 = DRIVER
+		
+		loginPage2.btnToolbarLogin_click();
+		loginPage2.login("bob@gmail.com", "bob123");
+		
+		loginPage1.btnToolbarLogin_click();
+		loginPage1.login("john@gmail.com", "john123");
+		
+		PassengerHome passengerHome = new PassengerHome(webdriver);	
+		passengerHome.enterDepartureDestination("Novi Sad", "Beograd");
+		
+		try {
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
