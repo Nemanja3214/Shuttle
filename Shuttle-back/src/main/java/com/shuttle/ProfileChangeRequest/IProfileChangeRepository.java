0dbdd5371891f06e49f1b159953967b29b728ba7
SV 10/2020 <nemanja.majstorovic3214@gmail.com>
2023-02-01 00:29:54 +0100

    Added profile change request and approving

---
+++ Shuttle-back/src/main/java/com/shuttle/ProfileChangeRequest/IProfileChangeRepository.java
@@ -0,0 +1,7 @@
+package com.shuttle.ProfileChangeRequest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProfileChangeRepository extends JpaRepository<ProfileChangeRequest, Long>{
+
+}
